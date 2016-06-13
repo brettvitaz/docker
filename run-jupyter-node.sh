8836b26d546ebff15736b084e6a18c46f11fe3c6
Brett Vitaz <vitazbt@uw.edu>
2016-06-13 12:52:03 -0700

    allow user to specify host directory to map as "/notebook"

--- run-jupyter-node.sh
+++ run-jupyter-node.sh
@@ -5,4 +5,4 @@ set -e
 ./build-python.sh
 ./build-jupyter.sh
 ./build-jupyter-node.sh
-docker run -d -p 8888:8888 --name jupyter-node jupyter-node
+docker run -d -p 8888:8888 --name jupyter-node ${1:+-v $1:/notebook }jupyter-node
